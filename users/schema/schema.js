9b77ecfe12d2a9e00558609103a492a24f047e0e
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-07-06 17:31:07 -0500

    setting up db.json for our database

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -10,7 +10,7 @@ const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 const users = [
     { id: '23', firstName: 'Bill', age: 20 },
     { id: '47', firstName: 'Samantha', age: 21 }
-]
+];
 
 const UserType = new GraphQLObjectType(
     {
