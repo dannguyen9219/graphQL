b82813009a86a26cea6a212e4ef211fcf77f481f
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-07-07 09:39:21 -0500

    ability to delete and edit users in mutation function

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -4,9 +4,9 @@ Root Query - allows us to jump into our graph to get data; entry point to our da
 */
 
 const graphql = require('graphql');
-// const _ = require('lodash'); // Helper library that allows us to walk through collections of data; don't neet lodash if using db.json because lodash is used for static data
+// const _ = require('lodash'); // Helper library that allows us to walk through collections of data; don't need lodash if using db.json because lodash is used for static data
 const axios = require('axios');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLNonNull } = graphql;
 
 // const users = [
 //     { id: '23', firstName: 'Bill', age: 20 },
@@ -75,8 +75,53 @@ const RootQuery = new GraphQLObjectType(
 // Resolve function purpose is actually going into the data and getting that information
 // The code/query that we write in GraphiQL in the browser gets sent to the RootQuery. The query went and found the user key inside of its field's object. It finds the data and then return the response object.
 
+const mutation = new GraphQLObjectType(
+    {
+        name: 'Mutation',
+        fields: {
+            addUser: {
+                type: UserType,
+                args: {
+                    firstName: { type: new GraphQLNonNull(GraphQLString) },
+                    age: { type: new GraphQLNonNull(GraphQLInt) },
+                    companyId: { type: GraphQLString }
+                },
+                resolve(parentValue, { firstName, age} ) {
+                    return axios.post('http://localhost:3000/users', { firstName, age })
+                    .then(res => res.data);
+                }
+            },
+            deleteUser: {
+                type: UserType,
+                args: {
+                    id: { type: new GraphQLNonNull(GraphQLString) }
+                },
+                resolve(parentValue, { id } ) {
+                    return axios.delete(`http://localhost:3000/users/${id}`, { id })
+                    .then(res => res.data);
+                }
+            },
+            editUser: {
+                type: UserType,
+                args: {
+                    id: { type: new GraphQLNonNull(GraphQLString) },
+                    firstName: { type: GraphQLString },
+                    age: { type: GraphQLInt },
+                    companyId: { type: GraphQLString }
+                },
+                resolve(parentValue, args) {
+                    return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+                    .then(res => res.data);
+                }
+            }
+        }
+    }
+);
+
+
 module.exports = new GraphQLSchema(
     {
-        query: RootQuery
+        query: RootQuery,
+        mutation
     }
 );
